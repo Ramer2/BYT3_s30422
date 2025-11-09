d0813b374df3f8735b80e2a012a789d00d74785a
Andrii Sysoiev <sisoev2006a@gmail.com>
2025-11-09 16:39:06 +0100

    Implemented Calculator and tested in Program.cs

--- s30422_assignment3_calculator/Calculator.cs
+++ s30422_assignment3_calculator/Calculator.cs
@@ -2,5 +2,44 @@
 
 public class Calculator
 {
-    
+    private double a;
+    private double b;
+    private string _operator;
+
+    public Calculator(double a, double b, string @operator)
+    {
+        this.a = a;
+        this.b = b;
+        _operator = @operator.Trim();
+    }
+
+    public double Execute()
+    {
+        switch (_operator)
+        {
+            case "+":
+            {
+                return a + b;
+            }
+            case "-":
+            {
+                return a - b;
+            }
+            case "*":
+            {
+                return a * b;
+            }
+            case "/":
+            {
+                if (b == 0) 
+                    throw new DivideByZeroException();
+                
+                return a / b;
+            }
+            default:
+            {
+                throw new ArgumentException($"Unknown operator \"{_operator}\"");
+            }
+        }
+    }
 }
--- s30422_assignment3_calculator/Program.cs
+++ s30422_assignment3_calculator/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using s30422_assignment3_calculator;
+
+// addiing two numbers
+var adder = new Calculator(4, 5, "+");
+Console.WriteLine(adder.Execute());
+
+// subtracting two numbers
+var subtractor = new Calculator(6, 9, "-");
+Console.WriteLine(subtractor.Execute());
+
+// multiplying two numbers
+var multiplier = new Calculator(1, 2, "*");
+Console.WriteLine(multiplier.Execute());
+
+// dividing two numbers
+var divider = new Calculator(3, 6, "/");
+Console.WriteLine(divider.Execute());
+
+// didivng by zero
+var dividerByZero = new Calculator(1, 0, "/");
+try
+{
+    dividerByZero.Execute();
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Caught division by zero exception: {ex.Message}");
+}
+
+// unknown opperation
+var unknownOperatoion = new Calculator(10, 10, "whatever");
+try
+{
+    unknownOperatoion.Execute();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Caught argument exception: {ex.Message}");
+}
